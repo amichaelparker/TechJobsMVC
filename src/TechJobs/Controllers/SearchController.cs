5e66ab446cde34dbd38a3e0a2b2b9bb0d48d3205
Adam Parker <amichaelparker@gmail.com>
2017-03-15 22:57:38 -0500

    Changes added through bonus objectives

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -4,17 +4,40 @@ using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
-    public class SearchController : Controller
+    public class SearchController : TechJobsController
     {
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
+            ViewBag.type = "all";
             return View();
         }
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
 
+            if (searchTerm == null)
+            {
+                ViewBag.type = searchType;
+                ViewBag.column = searchType;
+                ViewBag.error = "Please enter a search query.";
+                return View("Index");
+            }
+            if (searchType == "all")
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            } else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            ViewBag.search = searchTerm;
+            ViewBag.type = searchType;
+            ViewBag.error = "";
+            return View("Index");
+        }
     }
 }
---
+++ src/TechJobs/Controllers/TechJobsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechJobs.Controllers
+{
+    public class TechJobsController : Controller
+    {
+        internal static List<string> actionChoices = new List<string>();
+
+        static TechJobsController()
+        {
+            actionChoices.Add("Search");
+            actionChoices.Add("List");
+        }
+
+        public override ViewResult View()
+        {
+            ViewBag.actions = actionChoices;
+            return base.View();
+        }
+
+        public override ViewResult View(string url)
+        {
+            ViewBag.actions = actionChoices;
+            return base.View(url);
+        }
+    }
+}
